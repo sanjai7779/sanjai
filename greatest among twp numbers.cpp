d7db67840400cd741b0de2906b3de0b7e636e733
sanjai7779 <113322953+sanjai7779@users.noreply.github.com>
2022-09-13 09:59:52 +0530

    Add files via upload

---
+++ greatest among twp numbers.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=3, b=2;
+	if(a>b)
+	{
+		cout<<"a is greater";
+	}
+	if(a>b)
+	{
+		cout<<"a is greater";
+	}
+	else
+	{
+		cout<<"b is greater";
+	}
+	if(a>b)
+	{
+		cout<<"a is greater";
+	}
+	else if(b>a) 
+	{
+		cout<<"b is greater";
+	}
+	else
+	{
+		cout<<"both are equal";
+	}
+	return 0;
+}
+
