e7e25eaff4e8d40f75f0cadc444ed4d179beeda0
sanjai7779 <113322953+sanjai7779@users.noreply.github.com>
2022-09-14 14:39:12 +0530

    Add files via upload

---
+++ I love programming.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class programming
+{
+	string x="cpp";
+	public:
+		programming ();
+		programming(string);
+		void display();
+	};
+	int main()
+	{
+		programming p;
+		p.display();
+		programming :: display;
+		{
+			cout<<"I LOVE programming ";
+			programming :: programming string y;
+			{
+				cout<<" i love programming"+ y;	}
+				{ amount x;
+			}
+				int add(amount,x);
+				}
+				{
+			    return amount + x;
+			}
+			
+				
+				
+		
+	
